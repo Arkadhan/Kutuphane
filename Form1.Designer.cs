dbcecda9670a430110362191b341ce2abeb6c54b
arkadhan <hakanbvolkan@gmail.com>
2024-08-22 18:18:27 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Kutuphane
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbEkle = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.tbSil = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.tbGuncelle = new System.Windows.Forms.TextBox();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbEkle
+            // 
+            this.tbEkle.Location = new System.Drawing.Point(55, 31);
+            this.tbEkle.Name = "tbEkle";
+            this.tbEkle.Size = new System.Drawing.Size(100, 20);
+            this.tbEkle.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 165);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(666, 95);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(55, 71);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnEkle);
+            this.groupBox1.Controls.Add(this.tbEkle);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ekleme Bölümü";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tbSil);
+            this.groupBox2.Controls.Add(this.btnSil);
+            this.groupBox2.Location = new System.Drawing.Point(237, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Silme Bölümü";
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(55, 71);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 2;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // tbSil
+            // 
+            this.tbSil.Location = new System.Drawing.Point(47, 31);
+            this.tbSil.Name = "tbSil";
+            this.tbSil.Size = new System.Drawing.Size(100, 20);
+            this.tbSil.TabIndex = 3;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.tbGuncelle);
+            this.groupBox3.Controls.Add(this.btnGuncelle);
+            this.groupBox3.Location = new System.Drawing.Point(478, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 100);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Güncelleme Bölümü";
+            // 
+            // tbGuncelle
+            // 
+            this.tbGuncelle.Location = new System.Drawing.Point(47, 31);
+            this.tbGuncelle.Name = "tbGuncelle";
+            this.tbGuncelle.Size = new System.Drawing.Size(100, 20);
+            this.tbGuncelle.TabIndex = 3;
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(55, 71);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(75, 23);
+            this.btnGuncelle.TabIndex = 2;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button1.FlatAppearance.BorderSize = 53;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Image = global::Kutuphane.Properties.Resources.Ekran_görüntüsü_2024_08_22_172322;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(396, 266);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(272, 68);
+            this.button1.TabIndex = 6;
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 483);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbEkle;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.TextBox tbSil;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox tbGuncelle;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kutuphane
+{
+    public partial class Form1 : Form
+    {
+        KutuphaneDBEntities db = new KutuphaneDBEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void Vericek()
+        {
+            var liste = db.KitapTable.ToList();
+            listBox1.DataSource = liste;
+            listBox1.DisplayMember = "KitapAdi";
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Vericek();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            KitapTable kitap = new KitapTable();
+            kitap.KitapAdi = tbEkle.Text;
+
+            db.KitapTable.Add(kitap);
+            db.SaveChanges();
+            Vericek();
+            MessageBox.Show("Veri tabanına kitap eklendi", "Ekleme", MessageBoxButtons.OK);
+            tbEkle.Text = "";
+            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            var kitap = listBox1.SelectedItem as KitapTable;
+
+            tbSil.Text = kitap.KitapAdi.ToString();
+
+            if (kitap != null)
+            {
+                DialogResult cevap = MessageBox.Show(kitap.KitapAdi + " Kitabı silmek istediğinizden emin misiniz?", "Silme Onayı", MessageBoxButtons.YesNo);
+
+                if (cevap == DialogResult.Yes)
+                {
+                    db.KitapTable.Remove(kitap);
+                    db.SaveChanges();
+                    Vericek();
+                    MessageBox.Show("Veri tabanından kitap başarıyla silindi", "Silindi", MessageBoxButtons.OK);
+                    listBox1.SelectedIndex = listBox1.Items.Count - 1;
+                }
+            }
+
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            var kitap = listBox1.SelectedItem as KitapTable; 
+            
+            if (kitap != null)
+            {
+                kitap.KitapAdi=tbGuncelle.Text;
+                db.SaveChanges();
+                Vericek();
+                MessageBox.Show("Veri tabanından kitap başarıyla güncellendi", "Güncelleme", MessageBoxButtons.OK);
+                listBox1.SelectedIndex = listBox1.Items.Count - 1;
+            }
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var kitap=listBox1.SelectedItem as KitapTable;
+
+            if (kitap != null)
+            {
+                tbGuncelle.Text=kitap.KitapAdi;
+                tbSil.Text = kitap.KitapAdi;                   
+            }
+            else {
+                tbGuncelle.Text = "";
+                tbSil.Text = "";
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Form2 form = new Form2();
+            form.Show();
+            this.Hide();
+
+        }
+    }
+}
